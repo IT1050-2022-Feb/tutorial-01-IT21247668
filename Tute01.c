f38c9dafcf5a7d4b885ae1143bb26a4c199e37f4
Dilshan Dananjaya <99714824+IT21247668@users.noreply.github.com>
2022-02-26 15:20:12 +0000

    IT21247668

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,15 @@
 #include <stdio.h>
 
 int main() {
+  float sub1,sub2,avg;
+  printf("Input subject 1 marks: ");
+  scanf("%f",&sub1);
+
+  printf("Input subject 2 marks: ");
+  scanf("%f",&sub2);
+
+  avg=(sub1+sub2)/2.0;
+  printf("Average is: %.2f",avg);
   
   return 0;
 }
